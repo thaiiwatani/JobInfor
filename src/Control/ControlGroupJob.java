7f0e901a1c769bc615cda6b35bc1ce4a69175b26
J1637009 <J1637009@TBS.LOCAL>
2017-11-30 12:48:16 +0900

    V1
    V2

---
+++ src/Control/ControlGroupJob.java
@@ -0,0 +1,34 @@
+package Control;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import dataConnect.DataGroupJob;
+import entity.GroupJob;
+
+public class ControlGroupJob {
+	
+	private DataGroupJob dGroup = new DataGroupJob();
+	public boolean add(GroupJob g) throws SQLException
+	{
+		return dGroup.addGroupJob(g);
+	}
+	public List<GroupJob> loadData() throws SQLException
+	{
+		return dGroup.loadData();
+	}
+	public boolean delete(GroupJob g) throws SQLException
+	{
+		return dGroup.delGroupJob(g);
+	}
+	public boolean update(GroupJob g) throws SQLException
+	{
+		return dGroup.updateGroupJob(g);
+	}
+	public boolean checkName(GroupJob g) throws SQLException
+	{
+		return dGroup.checkName(g);
+	}
+
+}
+
---
+++ src/dataConnect/DataGroupJob.java
@@ -0,0 +1,187 @@
+package dataConnect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.GroupJob;
+
+
+public class DataGroupJob {
+	public DataGroupJob()
+	{
+		try {
+			loadData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DataGroupJob dtGroupJob = new DataGroupJob();
+
+	}
+	public List<GroupJob> loadData() throws SQLException
+	{
+		Connection con;
+		try {
+		con = new Connect().getMySQLConnection();
+		Statement statement = con.createStatement();
+		 
+	    String sql = "Select id, groupname, memo from group_jobs";
+	    ResultSet rs;
+		
+		rs = statement.executeQuery(sql);
+		//List<groupJob> lst = new ArrayList<>(groupJob);
+		List<GroupJob> lstGroupJob = new ArrayList<GroupJob>();
+		
+	      while (rs.next()) {
+	          int id = rs.getInt(1);
+	          String groupname = rs.getString(2);
+	          String memo = rs.getString("memo");
+	          GroupJob gJob = new GroupJob(id,groupname,memo);
+	          lstGroupJob.add(gJob);
+	          
+	          System.out.println("Id:" + id);
+	          System.out.println("groupname:" + groupname);
+	          System.out.println("memo:" + memo);
+	      }
+	      
+	      con.close();
+	      return lstGroupJob;
+	      
+		} catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return null;
+		}
+	}
+	public boolean addGroupJob(GroupJob g) throws SQLException
+	{
+		boolean check =false;
+		Connection con=null;
+		PreparedStatement preStmt = null;
+		try {
+		con = new Connect().getMySQLConnection();
+		String sql = "insert into group_jobs(GroupName,memo) values (?,?)";
+		preStmt = con.prepareStatement(sql);
+        preStmt.setString(1, g.getGroupName());
+        preStmt.setString(2, g.getMemo());
+        check =preStmt.execute();
+	    
+		}catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return check;
+		}
+		finally {
+            if (null != preStmt) {
+                preStmt.close();
+            }
+            if (null != con) {
+                con.close();
+            }
+		}
+		
+		return check;
+	}
+	public boolean checkName(GroupJob g) throws SQLException
+	{
+		boolean check =false;
+		Connection con=null;
+		PreparedStatement preStmt = null;
+		try {
+		con = new Connect().getMySQLConnection();
+		String sql = "Select id from group_jobs where GroupName =?";
+		preStmt = con.prepareStatement(sql);
+        preStmt.setString(1, g.getGroupName());
+        ResultSet rs;
+        rs= preStmt.executeQuery();
+        
+        check =rs.next();
+	    
+		}catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return check;
+		}
+		finally {
+            if (null != preStmt) {
+                preStmt.close();
+            }
+            if (null != con) {
+                con.close();
+            }
+		}
+		
+		return check;
+	}
+	public boolean delGroupJob(GroupJob gJob) throws SQLException
+	{
+		boolean check =false;
+		Connection con=null;
+		PreparedStatement preStmt = null;
+		try {
+		con = new Connect().getMySQLConnection();
+		String sql = "delete from group_jobs where id = ?";
+		preStmt = con.prepareStatement(sql);
+        preStmt.setInt(1, gJob.getId());
+        
+        check =preStmt.execute();
+	    
+		}catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return check;
+		}
+		finally {
+            if (null != preStmt) {
+                preStmt.close();
+            }
+            if (null != con) {
+                con.close();
+            }
+		}
+		
+		return check;
+	}
+	
+	public boolean updateGroupJob(GroupJob gJob) throws SQLException
+	{
+		boolean check =false;
+		Connection con=null;
+		PreparedStatement preStmt = null;
+		try {
+		con = new Connect().getMySQLConnection();
+		String sql = "update group_jobs set groupname = ?,memo=?  where id = ?)";
+		preStmt = con.prepareStatement(sql);
+		preStmt.setString(1, gJob.getGroupName());
+		preStmt.setString(2, gJob.getMemo());
+        preStmt.setInt(3, gJob.getId());     
+        check =preStmt.execute();
+	    
+		}catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return check;
+		}
+		finally {
+            if (null != preStmt) {
+                preStmt.close();
+            }
+            if (null != con) {
+                con.close();
+            }
+		}
+		
+		return check;
+	}
+
+}
+
